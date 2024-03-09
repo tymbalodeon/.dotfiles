f614ff6dcae656e01878a6986edb882356afbb5f
Ben Rosen <benjamin.j.rosen@gmail.com>
2024-03-09 09:09:28 -0500

    remove emacs

--- home.nix
+++ home.nix
@@ -16,9 +16,6 @@
       ".config/kitty/current-theme.conf".source = ./kitty/current-theme.conf;
       ".config/waybar/config.jsonc".source = ./waybar/config.jsonc;
       ".config/waybar/style.css".source = ./waybar/style.css;
-      # ".doom.d/config.el".source = ./emacs/config.el;
-      # ".doom.d/init.el".source = ./emacs/init.el;
-      # ".doom.d/packages.el".source = ./emacs/packages.el;
     };
 
     # homeDirectory = "/home/benrosen";
@@ -28,7 +25,6 @@
       bat
       dejavu_fonts
       delta
-      # emacs
       eza
       fd
       fh
