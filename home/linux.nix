6bde2ac5a5878a3c82ec2d930b277df768eb0042
Ben Rosen <benjamin.j.rosen@gmail.com>
2024-03-09 21:14:59 -0500

    fix linux imports

--- home/linux.nix
+++ home/linux.nix
@@ -5,11 +5,11 @@
 
   home = {
     file = {
-      ".config/hypr/hyprland.conf".source = ./hypr/hyprland.conf;
-      ".config/hypr/hyprpaper.conf".source = ./hypr/hyprpaper.conf;
-      ".config/hypr/wallpaper".source = ./hypr/wallpaper;
-      ".config/waybar/config.jsonc".source = ./waybar/config.jsonc;
-      ".config/waybar/style.css".source = ./waybar/style.css;
+      ".config/hypr/hyprland.conf".source = ../hypr/hyprland.conf;
+      ".config/hypr/hyprpaper.conf".source = ../hypr/hyprpaper.conf;
+      ".config/hypr/wallpaper".source = ../hypr/wallpaper;
+      ".config/waybar/config.jsonc".source = ../waybar/config.jsonc;
+      ".config/waybar/style.css".source = ../waybar/style.css;
     };
 
     homeDirectory = "/home/benrosen";
