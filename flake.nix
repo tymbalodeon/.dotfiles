c92a743de1433da8b6e192d57f80530858d665b3
Ben Rosen <benjamin.j.rosen@gmail.com>
2024-03-18 22:30:18 -0400

    add devshell

--- flake.nix
+++ flake.nix
@@ -15,7 +15,22 @@
     home-manager,
     nixpkgs,
     ...
-  } @ inputs: {
+  } @ inputs: let
+    supportedSystems = ["x86_64-darwin" "x86_64-linux"];
+
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems
+      (system: f {pkgs = import nixpkgs {inherit system;};});
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          alejandra
+          pre-commit
+        ];
+      };
+    });
+
     homeConfigurations = let
       hosts = ["benrosen" "work"];
 
