f6e0b0c4cb0aead4640555509d0ec4d66733f7b2
Ben Rosen <benjamin.j.rosen@gmail.com>
2024-05-04 17:01:36 -0400

    linux: update tuigreet

--- linux/configuration.nix
+++ linux/configuration.nix
@@ -67,7 +67,14 @@
       enable = true;
 
       settings.default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        command = ''
+          ${pkgs.greetd.tuigreet}/bin/tuigreet \
+            --asterisks \
+            --cmd Hyprland \
+            --remember \
+            --time
+        '';
+
         user = "greeter";
       };
     };
