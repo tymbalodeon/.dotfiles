d1a99df03d4986117bb0f0fd5708ae2f61826f31
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-01-05 17:29:29 -0500

    feat: use fira code

--- home.nix
+++ home.nix
@@ -69,9 +69,6 @@
       markdown-oxide
       marksman
       nb
-      nerd-fonts.caskaydia-cove
-      nerd-fonts.fira-mono
-      nerd-fonts.iosevka-term
       nil
       nix-search-cli
       nodePackages.bash-language-server
@@ -144,13 +141,17 @@
 
     kitty = {
       enable = true;
-      extraConfig = "map kitty_mod+enter launch --cwd=current --type=window";
+      extraConfig = ''
+        map kitty_mod+enter launch --cwd=current --type=window
+        font_features FiraCodeRoman-Regular +zero +onum +cv30 +ss09 +cv25 +cv26 +cv32 +ss07
+        font_features FiraCodeRoman-SemiBold +zero +onum +cv30 +ss09 +cv25 +cv26 +cv32 +ss07
+      '';
 
       settings = {
         confirm_os_window_close = 0;
         enable_audio_bell = "no";
         enabled_layouts = "grid, stack, vertical, horizontal, tall";
-        font_family = "FiraMono Nerd Font Mono";
+        font_family = "Fira Code";
         inactive_text_alpha = "0.5";
         include = "theme.conf";
         tab_bar_edge = "top";
