8fae7198c8e942ad1027494c6ade9d13aff1aa00
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-05-03 13:32:17 -0400

    fix: clean up darwin configuration imports

--- configuration/systems/darwin/configuration.nix
+++ configuration/systems/darwin/configuration.nix
@@ -3,21 +3,10 @@
   isNixOS,
   ...
 }: {
-  home-manager = {
-    extraSpecialArgs = {inherit inputs isNixOS;};
-    users.benrosen = import ./home.nix;
-  };
-
+  home-manager.extraSpecialArgs = {inherit inputs isNixOS;};
   imports = [inputs.home-manager.darwinModules.home-manager];
 
   nix.enable = false;
-  # nix.extraOptions = ''
-  #   bash-prompt-prefix = (nix:$name)\040
-  #   experimental-features = flakes nix-command
-  #   extra-nix-path = nixpkgs=flake:nixpkgs
-  #   upgrade-nix-store-path-url = https://install.determinate.systems/nix-upgrade/stable/universal
-  # '';
-
   security.sudo.extraConfig = ''Defaults env_keep += "TERM TERMINFO"'';
 
   system = {
