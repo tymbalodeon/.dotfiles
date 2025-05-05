b192dd6b66deedc8d27da36ec52b38f915effdd8
Ben Rosen <benrosen@upenn.edu>
2025-05-04 21:28:16 -0400

    chore: add notes

--- configuration/modules/xmonad/default.nix
+++ configuration/modules/xmonad/default.nix
@@ -1,3 +1,5 @@
+# TODO: change alt-space to be dmenu/rofi to match mac's spotlight
+# TODO: find a replacement for the current alt-space (shift-alt-space?)
 {
   home.file = {
     ".config/xmonad/xmonad.hs".source = ./xmonad.hs;
