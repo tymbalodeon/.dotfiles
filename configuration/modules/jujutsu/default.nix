5482930e6c594744c5a39d8a04d5f0d61ba6b734
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-05-15 12:55:29 -0400

    chore: update dependencies

--- configuration/modules/jujutsu/default.nix
+++ configuration/modules/jujutsu/default.nix
@@ -1,10 +1,6 @@
 {
   programs.jujutsu = {
     enable = true;
-
-    settings = {
-      # ui.default-command = ["log"];
-      user.name = "Ben Rosen";
-    };
+    settings.user.name = "Ben Rosen";
   };
 }
