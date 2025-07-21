be03d08e384769fc599350c0be9b68deb52f4448
Ben Rosen <benrosen@upenn.edu>
2025-07-21 09:06:53 -0400

    feat: add wireguard to work linux

--- configuration/systems/linux/hosts/work/home.nix
+++ configuration/systems/linux/hosts/work/home.nix
@@ -5,6 +5,7 @@
     packages = with pkgs; [
       # TODO: find a way to add this to known programs
       teams-for-linux
+      wireguard-tools
       # FIXME: this doesn't work.
       # zoom-us
     ];
