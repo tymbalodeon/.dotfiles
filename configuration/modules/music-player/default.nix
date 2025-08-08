6173393bf7fa20435674c44ef028b60681142d65
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-08-08 15:20:08 -0400

    feat: add ncmpcpp bindings

--- configuration/modules/music-player/default.nix
+++ configuration/modules/music-player/default.nix
@@ -1,13 +1,38 @@
 {pkgs, ...}: {
   home.packages = with pkgs;
     [
+      mpc
       mpd
     ]
     ++ (
       if stdenv.isLinux
       then [rmpc]
-      else if stdenv.isDarwin
-      then [mpc ncmpcpp]
       else []
     );
+
+  programs.ncmpcpp = {
+    bindings = [
+      {
+        key = "j";
+        command = "scroll_down";
+      }
+
+      {
+        key = "k";
+        command = "scroll_up";
+      }
+
+      {
+        key = "J";
+        command = ["select_item" "scroll_down"];
+      }
+
+      {
+        key = "K";
+        command = ["select_item" "scroll_up"];
+      }
+    ];
+
+    enable = true;
+  };
 }
