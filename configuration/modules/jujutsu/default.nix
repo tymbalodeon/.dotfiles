a83e2c03320d4e9503904f362413880a9e263a49
Ben Rosen <benrosen@upenn.edu>
2025-08-11 11:20:09 -0400

    feat: clean up jujutsu default revset

--- configuration/modules/jujutsu/default.nix
+++ configuration/modules/jujutsu/default.nix
@@ -3,6 +3,8 @@
     enable = true;
 
     settings = {
+      revsets.log = "@ | ancestors(remote_bookmarks().., 2) | trunk()";
+
       ui = {
         default-command = "log";
         pager = "less -FRX";
