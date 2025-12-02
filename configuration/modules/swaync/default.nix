408a11e9a749f09e2e00c03ed3db8d6b6db140e0
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-12-01 20:24:59 -0500

    feat: shorten notification timeout

--- configuration/modules/swaync/default.nix
+++ configuration/modules/swaync/default.nix
@@ -2,6 +2,11 @@
   # TODO: tweak styles
   services.swaync = {
     enable = true;
-    settings.notification-icon-size = 64;
+
+    settings = {
+      notification-icon-size = 64;
+      timeout = 5;
+      timeout-low = 3;
+    };
   };
 }
