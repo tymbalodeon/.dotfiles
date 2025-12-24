67b58c3b58dd8f134ca053b6e7087569e7e7ae29
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-12-24 14:03:15 -0500

    fix: steam

--- configuration/nixos/steam/default.nix
+++ configuration/nixos/steam/default.nix
@@ -1,7 +1,8 @@
-{
+{pkgs, ...}: {
   programs.steam = {
     dedicatedServer.openFirewall = true;
     enable = true;
+    package = pkgs.steam.override {extraArgs = "-system-composer";};
     remotePlay.openFirewall = true;
   };
 }
