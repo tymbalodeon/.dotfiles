e01cac162016b22997533ecb777834dc2c0b4c3e
Ben Rosen <benrosen@upenn.edu>
2026-01-13 14:03:26 -0500

    chore: cleanup

--- configuration/home-manager/users/default.nix
+++ configuration/home-manager/users/default.nix
@@ -1,8 +1,8 @@
-{
+rec {
   email = "benjamin.j.rosen@gmail.com";
   githubUsername = "tymbalodeon";
   gitlabUsername = "benjaminrosen";
   name = "Ben Rosen";
-  nbRemote = "git@github.com:tymbalodeon/notes.git";
+  nbRemote = "git@github.com:${githubUsername}/notes.git";
   username = "benrosen";
 }
