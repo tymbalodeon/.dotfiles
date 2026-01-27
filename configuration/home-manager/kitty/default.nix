98e1de50c401065a870845006c510ae58e352e3b
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-01-26 19:51:41 -0500

    feat: allow overriding stylix font at host level

--- configuration/home-manager/kitty/default.nix
+++ configuration/home-manager/kitty/default.nix
@@ -14,10 +14,13 @@
     in {
       enable = true;
 
-      extraConfig = ''
-        font_features FiraCodeRoman-Regular +zero +onum +cv30 +ss09 +cv25 +cv26 +cv32 +ss07
-        font_features FiraCodeRoman-SemiBold +zero +onum +cv30 +ss09 +cv25 +cv26 +cv32 +ss07
-      '';
+      extraConfig =
+        if cfg.font_family == "Fira Code"
+        then ''
+          font_features FiraCodeRoman-Regular +zero +onum +cv30 +ss09 +cv25 +cv26 +cv32 +ss07
+          font_features FiraCodeRoman-SemiBold +zero +onum +cv30 +ss09 +cv25 +cv26 +cv32 +ss07
+        ''
+        else "";
 
       keybindings = {
         "ctrl+shift+h" = "launch --stdin-source=@screen_scrollback hx";
@@ -30,12 +33,13 @@
         then config.lib.nixGL.wrap pkgs.kitty
         else pkgs.kitty;
 
-      settings =
+      settings = let
+        inherit (lib) optionalAttrs;
+      in
         {
           confirm_os_window_close = 0;
           enable_audio_bell = "no";
           enabled_layouts = "grid, stack, vertical, horizontal, tall";
-          font_family = "Fira Code";
           font_size = cfg.font_size;
           inactive_text_alpha = 0.5;
           shell = "${pkgs.nushell}/bin/nu";
@@ -44,20 +48,31 @@
           tab_powerline_style = "slanted";
           wheel_scroll_multiplier = 1;
         }
-        // lib.optionalAttrs isDarwin {
+        // optionalAttrs (cfg.font_family != "") {font_family = cfg.font_family;}
+        // optionalAttrs isDarwin {
           hide_window_decorations = "yes";
           macos_quit_when_last_window_closed = "yes";
         }
-        // lib.optionalAttrs isLinux {kitty_mod = "ctrl+shift";};
+        // optionalAttrs isLinux {kitty_mod = "ctrl+shift";};
 
       themeFile = "Catppuccin-Mocha";
     };
   };
 
-  options.kitty = with lib; {
+  options.kitty = with lib;
+  with types; {
+    font_family = mkOption {
+      default = "Fira Code";
+
+      description = ''
+        Set the font family for kitty. Set to an empty string to use the default font provided by stylix.'';
+
+      type = str;
+    };
+
     font_size = mkOption {
       default = 8.0;
-      type = types.float;
+      type = float;
     };
   };
 }
