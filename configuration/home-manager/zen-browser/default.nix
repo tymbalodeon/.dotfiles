4ac713c54316f2ea4e0f6277f39d68dc1b250301
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-02-07 21:45:17 -0500

    feat: add zen browser home-manager shortcut

--- configuration/home-manager/zen-browser/default.nix
+++ configuration/home-manager/zen-browser/default.nix
@@ -37,24 +37,10 @@
           SearchEngines = {
             Add = [
               {
-                Alias = "@np";
-                IconURL = "https://wiki.nixos.org/favicon.ico";
-                Name = "nixpkgs";
-                URLTemplate = "https://search.nixos.org/packages?query={searchTerms}";
-              }
-
-              {
-                Alias = "@no";
-                IconURL = "https://wiki.nixos.org/favicon.ico";
-                Name = "NixOS options";
-                URLTemplate = "https://search.nixos.org/options?query={searchTerms}";
-              }
-
-              {
-                Alias = "@nw";
-                IconURL = "https://wiki.nixos.org/favicon.ico";
-                Name = "NixOS Wiki";
-                URLTemplate = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
+                Alias = "@hm";
+                IconURL = "https://home-manager-options.extranix.com/favicon.ico";
+                Name = "Home-Manager options";
+                URLTemplate = "https://home-manager-options.extranix.com/favicon.ico?release=master&query=&{searchTerms}";
               }
 
               {
@@ -62,6 +48,27 @@
                 IconURL = "https://noogle.dev/favicon.ico";
                 Name = "noogle";
                 URLTemplate = "https://noogle.dev/q?term={searchTerms}";
+              }
+
+              {
+                Alias = "@no";
+                IconURL = "https://wiki.nixos.org/favicon.ico";
+                Name = "NixOS options";
+                URLTemplate = "https://search.nixos.org/options?channel=unstable&query={searchTerms}";
+              }
+
+              {
+                Alias = "@np";
+                IconURL = "https://wiki.nixos.org/favicon.ico";
+                Name = "nixpkgs";
+                URLTemplate = "https://search.nixos.org/packages?channel=unstable&query={searchTerms}";
+              }
+
+              {
+                Alias = "@nw";
+                IconURL = "https://wiki.nixos.org/favicon.ico";
+                Name = "NixOS Wiki";
+                URLTemplate = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
               }
             ];
 
