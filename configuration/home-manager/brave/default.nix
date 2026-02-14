bc2ad1959b3f3ed1d529bc94a1930ef3da389ade
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-02-13 22:01:54 -0500

    feat: add extensions to brave

--- configuration/home-manager/brave/default.nix
+++ configuration/home-manager/brave/default.nix
@@ -1,6 +1,44 @@
 {pkgs, ...}: {
   programs.chromium = {
     enable = true;
+
+    extensions = map (extension: {inherit (extension) id;}) [
+      {
+        name = "adguard-adblocker";
+        id = "bgnkhhnnamicmpeenaelnjfhikgbkllg";
+      }
+
+      {
+        name = "darkreader";
+        id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";
+      }
+
+      {
+        name = "proton-pass";
+        id = "ghmbeldphafepmbegfdlkpapadhbakde";
+      }
+
+      {
+        name = "refined-github-";
+        id = "hlepfoohegkhhmjieoechaddaejaokhf";
+      }
+
+      {
+        name = "subscription-feed-filter";
+        id = "jpdngflnlekafjhdlcnijphhcmeibdoa";
+      }
+
+      {
+        name = "surfingkeys_ff";
+        id = "gfbliohnnapiefjpjlpjnehglfpaknnc";
+      }
+
+      {
+        name = "undistracted-main";
+        id = "pjjgklgkfeoeiebjogplpnibpfnffkng";
+      }
+    ];
+
     package = pkgs.brave;
   };
 }
