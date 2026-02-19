12bea4dcdd5ea168408eba23c9f076913faa2781
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-02-18 20:41:15 -0500

    chore: merge trunk

--- configuration/home-manager/tsundeoku/default.nix
+++ configuration/home-manager/tsundeoku/default.nix
@@ -1,9 +1,71 @@
 {
+  config,
+  lib,
   pkgs,
   tsundeoku,
   ...
 }: {
-  home.packages = [
-    tsundeoku.packages.${pkgs.stdenv.hostPlatform.system}.default
-  ];
+  config = {
+    home = {
+      file.".config/tsundeoku/tsundeoku.toml" = let
+        cfg = config.tsundeoku;
+
+        ignoredPaths = valueOrNull cfg.ignoredPaths ''
+          ignored_paths = [
+            ${lib.concatStringsSep "," cfg.ignoredPaths}
+          ]
+        '';
+
+        isEmpty = value: value.isNull || value == [];
+
+        sharedDirectories = valueOrNull cfg.sharedDirectories ''
+          shared_directories = [
+            ${lib.concatStringsSep "," cfg.sharedDirectories}
+          ]
+        '';
+
+        valueOrNull = option: value:
+          if isEmpty option
+          then null
+          else value;
+      in ''
+        ignored_paths = [
+          ${lib.concatStringsSep "," cfg.ignoredDirectories}
+        ]
+
+        shared_directories = [
+          ${lib.concatStringsSep "," cfg.sharedDirectories}
+        ]
+      '';
+
+      packages = [
+        tsundeoku.packages.${pkgs.stdenv.hostPlatform.system}.default
+      ];
+    };
+  };
+
+  options.tsundeoku = let
+    inherit (lib) mkOption types;
+  in
+    with types; {
+      ignoredPaths = mkOption {
+        default = [];
+        type = listOf str;
+      };
+
+      localDirectory = mkOption {
+        default = null;
+        type = str;
+      };
+
+      scheduleInterval = mkOption {
+        default = null;
+        type = str;
+      };
+
+      sharedDirectories = mkOption {
+        default = [];
+        type = listOf str;
+      };
+    };
 }
