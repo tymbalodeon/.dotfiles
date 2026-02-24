52ac9129dc9700c5005e391cf035bc9f4902ccff
Ben Rosen <benrosen@upenn.edu>
2026-02-24 10:01:10 -0500

    fix: helix clipboard on home-manager

--- configuration/home-manager/helix/default.nix
+++ configuration/home-manager/helix/default.nix
@@ -3,122 +3,124 @@
   hostType,
   lib,
   ...
-}:
-{
-  home.sessionVariables = {EDITOR = "hx";};
+}: let
+  inherit (lib) optionalAttrs;
+in
+  {
+    home.sessionVariables = {EDITOR = "hx";};
 
-  imports =
-    [
-      ./bash
-      ./json
-      ./markdown
-      ./nix
-      ./toml
-      ./txt
-      ./yaml
-    ]
-    ++ (
-      if hostType == "home-manager"
-      then []
-      else [
-        ../../stylix
+    imports =
+      [
+        ./bash
+        ./json
+        ./markdown
+        ./nix
+        ./toml
+        ./txt
+        ./yaml
       ]
-    );
+      ++ (
+        if hostType == "home-manager"
+        then []
+        else [
+          ../../stylix
+        ]
+      );
 
-  programs.helix = {
-    defaultEditor = true;
-    enable = true;
+    programs.helix = {
+      defaultEditor = true;
+      enable = true;
 
-    settings =
-      {
-        editor = {
-          bufferline = "multiple";
-          clipboard-provider = "wayland";
-          color-modes = true;
-          cursorline = true;
+      settings =
+        {
+          editor = {
+            bufferline = "multiple";
+            color-modes = true;
+            cursorline = true;
 
-          cursor-shape = {
-            insert = "bar";
-            select = "underline";
-          };
-
-          end-of-line-diagnostics = "hint";
-          file-picker.hidden = false;
-          indent-guides.render = true;
-          inline-diagnostics.cursor-line = "warning";
-          mouse = false;
-          shell = ["nu" "-c"];
-
-          statusline = {
-            center = [
-              "read-only-indicator"
-              "file-name"
-              "file-modification-indicator"
-              "spacer"
-              "file-type"
-            ];
-
-            left = ["mode" "spinner"];
-
-            mode = {
-              insert = "INSERT";
-              normal = "NORMAL";
-              select = "SELECT";
+            cursor-shape = {
+              insert = "bar";
+              select = "underline";
             };
 
-            right = [
-              "diagnostics"
-              "selections"
-              "primary-selection-length"
-              "register"
-              "position"
-              "total-line-numbers"
-              "position-percentage"
-              "file-encoding"
-            ];
+            end-of-line-diagnostics = "hint";
+            file-picker.hidden = false;
+            indent-guides.render = true;
+            inline-diagnostics.cursor-line = "warning";
+            mouse = false;
+            shell = ["nu" "-c"];
+
+            statusline = {
+              center = [
+                "read-only-indicator"
+                "file-name"
+                "file-modification-indicator"
+                "spacer"
+                "file-type"
+              ];
+
+              left = ["mode" "spinner"];
+
+              mode = {
+                insert = "INSERT";
+                normal = "NORMAL";
+                select = "SELECT";
+              };
+
+              right = [
+                "diagnostics"
+                "selections"
+                "primary-selection-length"
+                "register"
+                "position"
+                "total-line-numbers"
+                "position-percentage"
+                "file-encoding"
+              ];
+            };
+
+            whitespace.render = {
+              space = "all";
+              tab = "all";
+            };
           };
 
-          whitespace.render = {
-            space = "all";
-            tab = "all";
+          keys = let
+            space = {w.S-q = ":quit!";};
+          in {
+            normal = {
+              inherit space;
+
+              C-g = [":reset-diff-change"];
+              C-j = ["extend_to_line_bounds" "delete_selection" "paste_after"];
+
+              C-k = [
+                "extend_to_line_bounds"
+                "delete_selection"
+                "move_line_up"
+                "paste_before"
+              ];
+
+              esc = ["collapse_selection" "keep_primary_selection"];
+              X = ["extend_line_up" "extend_to_line_bounds"];
+            };
+
+            select = {
+              inherit space;
+
+              X = ["extend_line_up" "extend_to_line_bounds"];
+            };
           };
-        };
-
-        keys = let
-          space = {w.S-q = ":quit!";};
-        in {
-          normal = {
-            inherit space;
-
-            C-g = [":reset-diff-change"];
-            C-j = ["extend_to_line_bounds" "delete_selection" "paste_after"];
-
-            C-k = [
-              "extend_to_line_bounds"
-              "delete_selection"
-              "move_line_up"
-              "paste_before"
-            ];
-
-            esc = ["collapse_selection" "keep_primary_selection"];
-            X = ["extend_line_up" "extend_to_line_bounds"];
-          };
-
-          select = {
-            inherit space;
-
-            X = ["extend_line_up" "extend_to_line_bounds"];
-          };
-        };
-      }
-      // lib.optionalAttrs
-      ((hostType
-        == "home-manager")
-      || (config.stylix.theme == "catppuccin-mocha")) {
-        theme = "catppuccin_mocha";
-      };
-  };
-}
-// lib.optionalAttrs (hostType != "home-manager") {
-  stylix.targets.helix.enable = !(config.stylix.theme == "catppuccin-mocha");
-}
+        }
+        // optionalAttrs
+        ((hostType
+          == "home-manager")
+        || (config.stylix.theme == "catppuccin-mocha")) {
+          theme = "catppuccin_mocha";
+        }
+        // optionalAttrs (hostType == "nixos") {clipboard-provider = "wayland";};
+    };
+  }
+  // optionalAttrs (hostType != "home-manager") {
+    stylix.targets.helix.enable = !(config.stylix.theme == "catppuccin-mocha");
+  }
