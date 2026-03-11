0aceed3f46f7d3cd51d3ecdf5e02abd2b1c94358
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-03-11 09:15:34 -0400

    feat: allow multiple nb remotes

--- configuration/home-manager/nb/default.nix
+++ configuration/home-manager/nb/default.nix
@@ -10,12 +10,44 @@
     home = {
       # TODO: handle $VERBOSE and $DRY_RUN
       # TODO: is it possible to git pull the remote notes here?
-      activation.nb =
+      activation.nb = let
+        git = "${pkgs.git}/bin/git";
+      in
         lib.hm.dag.entryAfter ["writeBoundary"]
         ''
-          echo ${pkgs.nushell}/bin/nu
-          echo ${./activation.nu}
-          # ${lib.concatStringsSep " " cfg.remotes}
+          remotes=(${lib.concatStringsSep " " cfg.remotes})
+          nbHome="$HOME/.nb"
+          notebooks=$(ls $nbHome)
+          index=0
+
+          for remote in "''${remotes[@]}"; do
+            if [[ "$index" = 0 ]]; then
+              name=home
+            else
+              url=''${remote/git@/}
+              url=''${url/.com/}
+
+              read domain user name < <(
+                echo $url |
+                ${pkgs.gawk}/bin/awk --field-separator [/:] '{print $1, $(NF-1), $NF}'
+              )
+
+              name=''${name/.git/}
+              name=''${domain}-''${user}-''${name}
+            fi
+
+            if [[ ! " ''${notebooks[*]} " =~ [[:space:]]$name[[:space:]] ]]; then
+              directory="$nbHome/$name"
+
+              mkdir --parents $directory
+              cd $directory
+
+              ${git} init
+              ${git} remote add origin $remote
+            fi
+
+            index+=1
+          done
         '';
 
       file = {
--- configuration/hosts/nixos/unstable/ruzia/home.nix
+++ configuration/hosts/nixos/unstable/ruzia/home.nix
@@ -7,9 +7,4 @@
   ];
 
   music-player.musicDirectory = "/run/media/benrosen/G-DRIVE Thunderbolt 3/Music";
-
-  user.nbRemotes = [
-    "git@github.com:benjaminrosen/notes.git"
-    "git@github.com:tymbalodeon/notes.git"
-  ];
 }
