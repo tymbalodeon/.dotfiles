96a159a9e89666afa97072559bcf7d96a6a0b70f
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-03-19 13:37:13 -0400

    feat: add colors to rss

--- configuration/home-manager/rss/default.nix
+++ configuration/home-manager/rss/default.nix
@@ -11,7 +11,26 @@
     extraConfig = ''
       bind j everywhere down
       bind k everywhere up
+
+      color article            default default
+      color background         default default
+      color end-of-text-marker color8  default
+      color hint-description   default color8
+      color hint-separator     default color8
+      color info               color4  color8
+      color listfocus          color4 default
+      color listfocus_unread   color3  default
+      color listnormal         default default
+      color listnormal_unread  color2  default
+      color title              color14 color8
+
       feed-sort-order lastupdated
+
+      highlight article "^(Feed|Title|Author|Link|Date): .+" color4 default bold
+      highlight article "^(Feed|Title|Author|Link|Date):" color14 default bold
+      highlight article "\\((link|image|video)\\)" color8 default
+      highlight article "https?://[^ ]+" color4 default
+      highlight article "\[[0-9]+\]" color6 default bold
     '';
   };
 }
