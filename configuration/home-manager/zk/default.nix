4c07a9135f89b669917c9345dce43da23c18638a
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-03-21 22:22:45 -0400

    feat: sync nb dir with zk edit/new

--- configuration/home-manager/zk/default.nix
+++ configuration/home-manager/zk/default.nix
@@ -7,14 +7,27 @@
       use ${../nb/get-nb-dir.nu} get-nb-dir
 
       def --wrapped zk [...args: string] {
+        mut is_main_zk = false
+
         let args = if not (".zk" | path exists) {
+          $is_main_zk = true
+
           $args
           | append [--working-dir (get-nb-dir)]
         } else {
           $args
         }
 
-        SHELL=$"(^which bash)" ^zk ...$args
+        try {
+          SHELL=$"(^which bash)" ^zk ...$args
+
+          if $is_main_zk and ($args | first) in [edit new] {
+            if (git -C (get-nb-dir) status --short | is-not-empty) {
+              nb sync
+            }
+          }
+        }
+
       }
     '')
   ];
