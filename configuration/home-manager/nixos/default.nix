28852cce01a9baafa5942c9bc54f9255303b89fa
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-03-30 18:40:54 -0400

    chore: init hyprland

--- configuration/home-manager/nixos/default.nix
+++ configuration/home-manager/nixos/default.nix
@@ -1,41 +1,27 @@
 {pkgs, ...}: {
-  config = {
-    gtk = let
-      gnomeTheme = pkgs.gnome-themes-extra;
-    in {
-      enable = true;
+  gtk = let
+    gnomeTheme = pkgs.gnome-themes-extra;
+  in {
+    enable = true;
 
-      iconTheme = {
-        name = "Adwaita";
-        package = gnomeTheme;
-      };
+    iconTheme = {
+      name = "Adwaita";
+      package = gnomeTheme;
     };
+  };
 
-    home = {
-      packages = with pkgs; [
-        equibop
-        libnotify
-        wev
-      ];
+  home = {
+    packages = with pkgs; [
+      equibop
+      libnotify
+      wev
+    ];
 
-      pointerCursor = {
-        gtk.enable = true;
-        package = pkgs.bibata-cursors;
-        name = "Bibata-Modern-Classic";
-        size = 16;
-      };
-    };
-
-    stylix.targets.waybar.font = "sansSerif";
-
-    xdg = {
-      configFile."mimeapps.list".force = true;
-      enable = true;
-
-      mimeApps = {
-        defaultApplications."application/pdf" = ["org.pwmt.zathura.desktop"];
-        enable = true;
-      };
+    pointerCursor = {
+      gtk.enable = true;
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+      size = 16;
     };
   };
 
@@ -52,4 +38,16 @@
     ../swaync
     ../waybar
   ];
+
+  stylix.targets.waybar.font = "sansSerif";
+
+  xdg = {
+    configFile."mimeapps.list".force = true;
+    enable = true;
+
+    mimeApps = {
+      defaultApplications."application/pdf" = ["org.pwmt.zathura.desktop"];
+      enable = true;
+    };
+  };
 }
--- configuration/hosts/nixos/unstable/ruzia/home.nix
+++ configuration/hosts/nixos/unstable/ruzia/home.nix
@@ -4,6 +4,7 @@
   imports = [
     ../../../../home-manager
     ../../../../home-manager/music-player
+    ../../../../home-manager/niri
   ];
 
   music-player.musicDirectory = "/run/media/benrosen/G-DRIVE Thunderbolt 3/Music";
