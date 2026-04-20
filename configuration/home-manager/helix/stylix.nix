df794d0b59d443da44563eb84112829921325bc8
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-04-19 21:18:31 -0400

    feat: improve stylix helix theme

--- configuration/home-manager/helix/stylix.nix
+++ configuration/home-manager/helix/stylix.nix
@@ -26,19 +26,35 @@
                 "${base16-helix}/base16-${config.stylix.theme}.toml"
               )
               // {
-                "markup.heading.marker" = "";
                 "markup.heading.1" = "base09";
                 "markup.heading.2" = "base0A";
                 "markup.heading.3" = "base0B";
                 "markup.heading.4" = "base0C";
                 "markup.heading.5" = "base0D";
                 "markup.heading.6" = "base0E";
+                "markup.heading.marker" = "";
 
                 "ui.cursor.primary" = {
                   bg = "base0E";
                   fg = "base01";
                 };
 
+                "ui.bufferline" = {
+                  bg = "base00";
+                  fg = "base04";
+                };
+
+                "ui.bufferline.active" = {
+                  bg = "base00";
+                  fg = "base06";
+
+                  underline = {
+                    color = "base06";
+                    style = "line";
+                  };
+                };
+
+                "ui.bufferline.background" = {bg = "base00";};
                 "ui.gutter.selected" = {bg = "base01";};
                 "ui.virtual.indent-guide" = "base01";
                 "ui.virtual.whitespace" = "base01";
