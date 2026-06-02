ccdad67d21944be7595c049830e303bfbc856ffa
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-06-02 10:01:31 -0400

    fix: bashrc

--- configuration/home-manager/bash/default.nix
+++ configuration/home-manager/bash/default.nix
@@ -1,25 +1,27 @@
 {
   imports = [../eza];
 
-  programs.bash = {
-    enable = true;
-    historyControl = ["erasedups"];
-    historyIgnore = ["cd" "exit" "ls"];
+  programs = {
+    bash = {
+      bashrcExtra = "set -o vi";
+      enable = true;
+      historyControl = ["erasedups"];
+      historyIgnore = ["cd" "exit" "ls"];
 
-    # TODO: set vi mode cursors
-    initExtra = ''
-      set -o vi
+      # TODO: set vi mode cursors
+      initExtra = ''
+        PS1="\[\e[1m\e[1;36m\w\e[0m\]\n$ "
+        PS2="\[\e[1m\e[1;32m>\e[0m\] "
 
-      PS1="\[\e[1m\e[1;36m\w\e[0m\]\n$ "
-      PS2="\[\e[1m\e[1;32m>\e[0m\] "
-    '';
+      '';
 
-    shellAliases = {
-      l = "eza --long";
-      la = "eza --all --long";
-      ls = "eza --oneline";
-      lsa = "eza --all --oneline";
-      ssh = ". '${./ssh.sh}'";
+      shellAliases = {
+        l = "eza --long";
+        la = "eza --all --long";
+        ls = "eza --oneline";
+        lsa = "eza --all --oneline";
+        ssh = ". '${./ssh.sh}'";
+      };
     };
   };
 }
