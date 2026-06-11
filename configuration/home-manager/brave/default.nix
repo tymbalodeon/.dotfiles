290219db6f3a011393598ff0e4cb2e30c0bad95b
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-06-10 20:18:48 -0400

    feat: add desktop entry for Brave incognito

--- configuration/home-manager/brave/default.nix
+++ configuration/home-manager/brave/default.nix
@@ -1,15 +1,38 @@
 {pkgs, ...}: {
   home.packages = [pkgs.brave];
 
-  xdg.mimeApps = {
-    enable = true;
+  xdg = {
+    desktopEntries = {
+      brave-browser-incognito = {
+        categories = [
+          "Network"
+          "WebBrowser"
+        ];
 
-    defaultApplications = {
-      "text/html" = "brave-browser.desktop";
-      "x-scheme-handler/about" = "brave-browser.desktop";
-      "x-scheme-handler/http" = "brave-browser.desktop";
-      "x-scheme-handler/https" = "brave-browser.desktop";
-      "x-scheme-handler/unknown" = "brave-browser.desktop";
+        exec = "${pkgs.brave}/bin/brave --incognito %U";
+        genericName = "Web Browser";
+        icon = "brave-browser";
+        name = "Brave (Incognito)";
+
+        mimeType = [
+          "text/html"
+          "text/xml"
+        ];
+
+        terminal = false;
+      };
+    };
+
+    mimeApps = {
+      enable = true;
+
+      defaultApplications = {
+        "text/html" = "brave-browser.desktop";
+        "x-scheme-handler/about" = "brave-browser.desktop";
+        "x-scheme-handler/http" = "brave-browser.desktop";
+        "x-scheme-handler/https" = "brave-browser.desktop";
+        "x-scheme-handler/unknown" = "brave-browser.desktop";
+      };
     };
   };
 }
