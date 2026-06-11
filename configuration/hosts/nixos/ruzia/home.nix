c8f715ff31b585dab32746400b5439cb60cc1dad
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-06-11 16:01:45 -0400

    feat: use bigger kitty font on ruzia

--- configuration/hosts/nixos/ruzia/home.nix
+++ configuration/hosts/nixos/ruzia/home.nix
@@ -3,8 +3,10 @@
 
   imports = [
     ../../../home-manager
+    ../../../home-manager/kitty
     ../../../home-manager/music-player
   ];
 
+  kitty.fontSize = 12.0;
   music-player.musicDirectory = "/run/media/benrosen/G-DRIVE Thunderbolt 3/Music";
 }
