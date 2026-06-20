4f71e5a311ad807d6cbf53c7490b4806abc8baec
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-06-20 11:48:14 -0400

    chore: clean up font definitions

--- modules/home-manager/fonts/default.nix
+++ modules/home-manager/fonts/default.nix
@@ -1,34 +1,39 @@
 {pkgs, ...}: {
   fonts.fontconfig.enable = true;
+  home.packages = let
+    fonts = with pkgs; [
+      adwaita-fonts
+      andika
+      cantarell-fonts
+      dejavu_fonts
+      fanwood
+      fira
+      font-awesome
+      gentium
+      gentium-book
+      goudy-bookletter-1911
+      gyre-fonts
+      ibm-plex
+      inconsolata
+      inter
+      iosevka
+      jost
+      lato
+      liberation_ttf
+      nerd-fonts.iosevka-term
+      nerd-fonts.iosevka-term-slab
+      nerd-fonts.jetbrains-mono
+      noto-fonts
+      prociono
+      ubuntu-classic
+    ];
 
-  home.packages = with pkgs; [
-    adwaita-fonts
-    andika
-    cantarell-fonts
-    dejavu_fonts
-    fanwood
-    fira
-    font-awesome
-    font-manager
-    fontpreview
-    gentium
-    gentium-book
-    goudy-bookletter-1911
-    gyre-fonts
-    ibm-plex
-    inconsolata
-    inter
-    iosevka
-    jost
-    lato
-    liberation_ttf
-    nerd-fonts.iosevka-term
-    nerd-fonts.iosevka-term-slab
-    nerd-fonts.jetbrains-mono
-    noto-fonts
-    prociono
-    ubuntu-classic
-  ];
+    programs = with pkgs; [
+      font-manager
+      fontpreview
+    ];
+  in
+    fonts ++ programs;
 
   imports = [../shell/nushell];
   nushell.extraScripts = [{source = ./font.nu;}];
