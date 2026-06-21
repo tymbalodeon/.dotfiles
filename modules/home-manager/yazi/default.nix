9023ccca679e33e43a73f157f3085aa686aa9b43
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-06-21 15:55:11 -0400

    feat: add yazi settings

--- modules/home-manager/yazi/default.nix
+++ modules/home-manager/yazi/default.nix
@@ -3,6 +3,12 @@
     enable = true;
     enableBashIntegration = true;
     enableNushellIntegration = true;
+
+    settings.mgr = {
+      show_hidden = true;
+      sort_dir_first = false;
+    };
+
     shellWrapperName = "y";
   };
 }
