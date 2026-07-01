bb7123419fcb15ea6c5d62c7cebd651eecd6d67c
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-07-01 13:37:26 -0400

    feat: improve hyprlock styles

--- modules/home-manager/lock/default.nix
+++ modules/home-manager/lock/default.nix
@@ -1,21 +1,48 @@
 {
+  config,
+  lib,
+  ...
+}: {
   programs.hyprlock = {
     enable = true;
 
-    settings = {
+    settings = let
+      backgroundColor = color colors.base00;
+      color = base16Name: "rgb(${base16Name})";
+      colors = config.lib.stylix.colors;
+      foregroundColor = color colors.base05;
+    in {
       general.hide_cursor = true;
+
+      input-field = lib.mkForce [
+        {
+          check_color = color colors.base0C;
+          dots_size = 0.2;
+          fade_on_empty = false;
+          fail_color = color colors.base09;
+          font_color = foregroundColor;
+          font_family = config.stylix.fonts.sansSerif.name;
+          inner_color = backgroundColor;
+          outer_color = backgroundColor;
+          outline_thickness = 2;
+          placeholder_text = "<i>Input password...</i>";
+          size = "288, 48";
+        }
+      ];
 
       label = [
         {
-          font_size = 90;
-          position = "0, -25%";
+          color = foregroundColor;
+          font_size = 24;
+          position = "0, -10%";
           text = "$TIME12";
           valign = "top";
         }
 
         {
-          font_size = 25;
-          position = "0, -32%";
+          color = foregroundColor;
+          font_size = 16;
+          position = "0, -15%";
           text = ''cmd[update:43200000] date +"%A, %d %B %Y"'';
           valign = "top";
         }
