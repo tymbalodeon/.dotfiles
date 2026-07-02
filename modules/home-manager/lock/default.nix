8dcd4b417d0e1f7fec33248ceb97d0efb654eb4f
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-07-01 22:19:22 -0400

    feat: add lock options

--- modules/home-manager/lock/default.nix
+++ modules/home-manager/lock/default.nix
@@ -3,50 +3,63 @@
   lib,
   ...
 }: {
-  programs.hyprlock = {
-    enable = true;
+  config = {
+    programs.hyprlock = {
+      enable = true;
 
-    settings = let
-      backgroundColor = color colors.base00;
-      color = base16Name: "rgb(${base16Name})";
-      colors = config.lib.stylix.colors;
-      foregroundColor = color colors.base05;
-    in {
-      general.hide_cursor = true;
+      settings = let
+        color = base16Name: "rgb(${base16Name})";
+        colors = config.lib.stylix.colors;
+        foregroundColor = color colors.base05;
+      in {
+        general.hide_cursor = true;
 
-      input-field = lib.mkForce [
-        {
-          check_color = color colors.base0C;
-          dots_size = 0.2;
-          fade_on_empty = false;
-          fail_color = color colors.base09;
-          font_color = foregroundColor;
-          font_family = config.stylix.fonts.sansSerif.name;
-          inner_color = backgroundColor;
-          outer_color = backgroundColor;
-          outline_thickness = 2;
-          placeholder_text = "<i>Input password...</i>";
-          size = "288, 48";
-        }
-      ];
+        input-field = lib.mkForce [
+          {
+            check_color = color colors.base0C;
+            fade_on_empty = false;
+            fail_color = color colors.base09;
+            font_color = foregroundColor;
+            font_family = config.stylix.fonts.sansSerif.name;
+            inner_color = color colors.base01;
+            outer_color = foregroundColor;
+            outline_thickness = 2;
+            placeholder_text = "";
+            rounding = 1;
+            size = "288, 48";
+          }
+        ];
 
-      label = [
-        {
-          color = foregroundColor;
-          font_size = 48;
-          position = "0, -22%";
-          text = "$TIME12";
-          valign = "top";
-        }
+        label = [
+          {
+            color = foregroundColor;
+            font_size = 48;
+            position = config.lock.timePosition;
+            text = "$TIME12";
+            valign = "top";
+          }
 
-        {
-          color = foregroundColor;
-          font_size = 16;
-          position = "0, -30%";
-          text = ''cmd[update:43200000] date +"%A, %d %B %Y"'';
-          valign = "top";
-        }
-      ];
+          {
+            color = foregroundColor;
+            font_size = 16;
+            position = config.lock.datePosition;
+            text = ''cmd[update:43200000] date +"%A, %d %B %Y"'';
+            valign = "top";
+          }
+        ];
+      };
+    };
+  };
+
+  options.lock = {
+    datePosition = lib.mkOption {
+      default = "0, -30%";
+      type = lib.types.str;
+    };
+
+    timePosition = lib.mkOption {
+      default = "0, -22%";
+      type = lib.types.str;
     };
   };
 }
