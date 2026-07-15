2051b195b66c641c52d23a582a2f39f71f4fb2c9
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-07-15 14:02:38 -0400

    feat: where-is-my -> astronaut (sddm)

--- modules/nixos/display-manager/default.nix
+++ modules/nixos/display-manager/default.nix
@@ -6,51 +6,82 @@
 }: {
   config = let
     cfg = config.displayManager;
+    colors = config.lib.stylix.colors.withHashtag;
 
-    sddmTheme = let
-      backgroundColor = "${colors.base00}";
-      colors = config.lib.stylix.colors.withHashtag;
-      fontSize = 24;
-      foregroundColor = "${colors.base05}";
-    in
-      pkgs.where-is-my-sddm-theme.override
-      {
-        themeConfig.General = {
-          backgroundFill = backgroundColor;
-          basicTextColor = foregroundColor;
-          cursorBlinkAnimation = false;
-          hideCursor = true;
-          passwordCursorColor = foregroundColor;
-          passwordFontSize = fontSize;
-          passwordTextColor = foregroundColor;
-        };
+    sddm-astronaut = pkgs.sddm-astronaut.override {
+      themeConfig = {
+        BackgroundColor = colors.base00;
+        DateFormat = "dddd MMMM d yyyy";
+
+        DateTextColor = colors.base05;
+        DimBackground = 1.0;
+        DimBackgroundColor = colors.base00;
+        DropdownBackgroundColor = colors.base00;
+        DropdownSelectedBackgroundColor = colors.base01;
+        DropdownTextColor = colors.base05;
+        Font = config.stylix.fonts.sansSerif.name;
+
+        # FIXME: make an option and change on different hosts
+        FontSize = 16;
+
+        FormBackgroundColor = colors.base00;
+        HaveFormBackground = true;
+        HeaderTextColor = colors.base05;
+        HideVirtualKeyboard = true;
+        HighlightBackgroundColor = colors.base03;
+        HighlightBorderColor = colors.base03;
+        HighlightTextColor = colors.base05;
+        HourFormat = "h:mm AP";
+        HoverPasswordIconColor = colors.base07;
+        HoverSessionButtonTextColor = colors.base07;
+        HoverSystemButtonsIconsColor = colors.base07;
+        HoverUserIconColor = colors.base07;
+        HoverVirtualKeyboardButtonTextColor = colors.base07;
+        LoginButtonBackgroundColor = colors.base01;
+        LoginButtonTextColor = colors.base05;
+        LoginFieldBackgroundColor = colors.base02;
+        LoginFieldTextColor = colors.base05;
+        PartialBlur = false;
+        PasswordFieldBackgroundColor = colors.base02;
+        PasswordFieldTextColor = colors.base05;
+        PasswordIconColor = colors.base05;
+        PlaceholderTextColor = colors.base03;
+        SessionButtonTextColor = colors.base05;
+        SystemButtonsIconsColor = colors.base05;
+        TimeTextColor = colors.base05;
+        UseRealName = false;
+        UserIconColor = colors.base05;
+        VirtualKeyboardButtonTextColor = colors.base05;
+        WarningColor = colors.base0A;
       };
+    };
   in {
     environment.systemPackages = with pkgs; [
       bibata-cursors
-      sddmTheme
+      sddm-astronaut
     ];
 
     services.displayManager = let
-      cursorSize = 24;
       cursorTheme = "Bibata-Modern-Classic";
     in {
       inherit (cfg) defaultSession;
 
       sddm = {
         enable = true;
-        extraPackages = [sddmTheme];
+        extraPackages = [sddm-astronaut];
 
         settings = {
           AutoLogin.User = config.nixos.username;
 
           Theme = {
-            CursorSize = cursorSize;
+            # FIXME: pull from config.stylix.cursor.size
+            CursorSize = 24;
+
             CursorTheme = cursorTheme;
           };
         };
 
-        theme = "where_is_my_sddm_theme";
+        theme = "sddm-astronaut-theme";
         wayland.enable = true;
       };
     };
