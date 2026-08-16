d65a7f2aa90a7e3c786b4ebfd766b5d757c075a5
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-08-16 14:30:49 -0400

    feat: add printer GUI

--- modules/nixos/printing/default.nix
+++ modules/nixos/printing/default.nix
@@ -1,6 +1,14 @@
-{
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    system-config-printer
+  ];
+
   services = {
     ipp-usb.enable = true;
-    printing.enable = true;
+
+    printing = {
+      drivers = [pkgs.postscript-lexmark];
+      enable = true;
+    };
   };
 }
